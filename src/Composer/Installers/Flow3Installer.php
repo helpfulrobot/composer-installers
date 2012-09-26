2e0d2c4c5d66e41f89e9a0ea3e3fa4646ea61541
Karsten Dambekalns <karsten@typo3.org>
2012-09-26 11:21:57 +0300

    Tweak the way package install paths are inferred
    This enables the following ruleset:
    
    * flow3-package - Packages/Application/{$name}
    * flow3-plugin - Packages/Plugins/{$name}
    * flow3-site - Packages/Sites/{$name}
    * flow3-<something> - Packages/<Something>/{$name}

--- src/Composer/Installers/Flow3Installer.php
+++ src/Composer/Installers/Flow3Installer.php
@@ -1,17 +1,40 @@
 <?php
 namespace Composer\Installers;
 
-class Flow3Installer extends BaseInstaller
-{
-    protected $locations = array(
-        'framework'     => 'Packages/Framework/{$name}/',
-        'package'     => 'Packages/Application/{$name}/',
-    );
+/**
+ * An installer to handle FLOW3 specifics when installing packages.
+ */
+class Flow3Installer extends BaseInstaller {
 
-	public function inflectPackageVars($vars) {
-		$autoload = $this->package->getAutoload();
-		$namespace = key($autoload['psr-0']);
-		$vars['name'] = str_replace('\\','.',$namespace);
-		return $vars;
-	}
+    /**
+     * Modify the package name to be a FLOW3 style key.
+     *
+     * @param array $vars
+     * @return array
+     */
+    public function inflectPackageVars($vars) {
+
+        // infer package location from package type
+        $packageLocation = strtolower(substr($vars['type'], strpos($vars['type'], '-') + 1));
+        switch ($packageLocation) {
+            case 'package':
+                $this->locations['package'] = 'Packages/Application/{$name}/';
+                break;
+            case 'plugin':
+            case 'site':
+                $this->locations[$packageLocation] = 'Packages/' . ucfirst($packageLocation) . 's/{$name}/';
+                break;
+            default:
+                $this->locations[$packageLocation] = 'Packages/' . ucfirst($packageLocation) . '/{$name}/';
+                break;
+        }
+
+        $autoload = $this->package->getAutoload();
+        $namespace = key($autoload['psr-0']);
+        $vars['name'] = str_replace('\\', '.', $namespace);
+
+        return $vars;
+    }
 }
+
+?>
