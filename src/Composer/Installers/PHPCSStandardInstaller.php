185c41437c23852b787f761a5bcf2383f83702d3
Stefano Kowalke <blueduck@gmx.net>
2012-11-15 23:53:33 +0100

    Remove the test output.

--- src/Composer/Installers/PHPCSStandardInstaller.php
+++ src/Composer/Installers/PHPCSStandardInstaller.php
@@ -6,9 +6,4 @@ class PHPCSStandardInstaller extends BaseInstaller
     protected $locations = array(
         'standard' => 'squizlabs/php_codesniffer/CodeSniffer/Standards/{$name}',
     );
-
-    public function inflectPackageVars($vars)
-    {
-        echo $vars;
-    }
 }
