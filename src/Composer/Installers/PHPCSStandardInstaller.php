0dcd261040efc6470e0355bd6623083eeaf3dddb
Stefano Kowalke <blueduck@gmx.net>
2012-11-16 19:21:26 +0100

    Test if we need to split the standards into separate repositories.

--- src/Composer/Installers/PHPCSStandardInstaller.php
+++ src/Composer/Installers/PHPCSStandardInstaller.php
@@ -4,16 +4,18 @@ namespace Composer\Installers;
 class PHPCSStandardInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'standard' => 'vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/{$name}',
+        'standard' => 'vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/',
     );
 
     /**
      * Format package name
      */
+    /*
     public function inflectPackageVars($vars)
     {
         $vars['name'] = strtoupper($vars['name']);
 
         return $vars;
     }
+    */
 }
