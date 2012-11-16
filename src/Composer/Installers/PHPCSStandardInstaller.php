f28b59003614f34fe0797f82c4607c395d74da93
Stefano Kowalke <blueduck@gmx.net>
2012-11-16 19:27:17 +0100

    Revert "Test if we need to split the standards into separate repositories."
    This reverts commit 0dcd261040efc6470e0355bd6623083eeaf3dddb.

--- src/Composer/Installers/PHPCSStandardInstaller.php
+++ src/Composer/Installers/PHPCSStandardInstaller.php
@@ -4,18 +4,16 @@ namespace Composer\Installers;
 class PHPCSStandardInstaller extends BaseInstaller
 {
     protected $locations = array(
-        'standard' => 'vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/',
+        'standard' => 'vendor/squizlabs/php_codesniffer/CodeSniffer/Standards/{$name}',
     );
 
     /**
      * Format package name
      */
-    /*
     public function inflectPackageVars($vars)
     {
         $vars['name'] = strtoupper($vars['name']);
 
         return $vars;
     }
-    */
 }
